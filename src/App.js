9d372cd69609bd562cac7abace507a9ab744d0e2
Praveen Goswami <praveen828589@gmail.com>
2023-02-02 00:56:58 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import AnimalShow from "./components/AnimalShow";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function getRandomAnimal(){
+    const animals = ["cat", "dog", "cow", "gator", "horse", "bird"];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
+function App(){
+    const [animals, setAnimals] = useState([]);
+
+    const handleClick = () => {
+        setAnimals([...animals, getRandomAnimal()]);
+    }
+
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />
+    });
+
+    return <div className='App'>
+        <button onClick={handleClick} className="btn">Add Animal</button>
+        <div className="animals-container">
+            {renderedAnimals}
+        </div>
     </div>
-  );
 }
 
 export default App;
---
+++ src/components/AnimalShow.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import './AnimalShow.css';
+import bird from '../svg/bird.svg';
+import cat from '../svg/cat.svg';
+import cow from '../svg/cow.svg';
+import dog from '../svg/dog.svg';
+import gator from '../svg/gator.svg';
+import heart from '../svg/heart.svg';
+import horse from '../svg/horse.svg';
+
+const animalImage = {
+    bird,
+    cat,
+    cow,
+    dog,
+    gator,
+    horse
+}
+
+function AnimalShow({ type }){
+    const [clicks, setClicks] = useState(0);
+
+    const handleClick = () => {
+        setClicks(clicks + 1);
+    }
+
+    return (
+        <div className='animal-show' onClick={handleClick}>
+            <img src={animalImage[type]} alt={type} className="animal"/>
+            <img 
+                src={heart} 
+                alt="heart"
+                className="heart"
+                style={{ width : 15 + 10 * clicks + 'px'}}
+            />
+        </div>
+    )
+}
+
+export default AnimalShow;
